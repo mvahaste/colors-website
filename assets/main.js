85e8625eca02999919befa6e5a1fce2950c23dda
M <48407846+mvahaste@users.noreply.github.com>
2022-11-04 01:10:09 +0200

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,26 @@
+// Autor: Mikk Vahaste
+//
+// Muutujad elementide jaoks
 var searchBar = document.getElementById("search");
 var grid = document.getElementsByClassName("grid-item");
 
-searchBar.addEventListener("keyup", () => {
-    searchItems()
-});
-
+// Otsingufunktsioon
 function searchItems() {
+    // Käib üle iga elemendi
     for (var item of grid) {
-        console.log(searchBar.value.length)
+        // Kui päring on / ei ole elemendi <p></p> elemendi sees-
         if (item.getElementsByTagName("p")[0].innerHTML.includes(searchBar.value.toUpperCase())) {
+            // -ära peida elementi
             item.classList.remove("hidden");
         } else {
+            // -peida element
             item.classList.add("hidden");
         }
     }
 }
+
+// Event listener, et käivitata funktsioon igal klahvivajutusel, et otsimine oleks automaatne
+searchBar.addEventListener("keyup", () => {
+    searchItems()
+});
+
