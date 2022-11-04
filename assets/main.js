18b7a351923960c078ea57ff333d22c12f9b6a6d
M <48407846+mvahaste@users.noreply.github.com>
2022-11-04 12:48:48 +0200

    *

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,33 @@
+// Autor: Mikk Vahaste
+//
+// Muutujad elementide jaoks
 var searchBar = document.getElementById("search");
 var grid = document.getElementsByClassName("grid-item");
+var noResultsText = document.getElementsByClassName("no-results")[0];
 
+// Otsingufunktsioon
 function searchItems() {
+    // Käib üle iga elemendi
     for (var item of grid) {
-        if (item.getElementsByTagName("p")[0].innerHTML.includes(searchBar.value.toUpperCase())) {
+        // Kui päring on / ei ole elemendi <p></p> elemendi sees-
+        if (item.getElementsByTagName("p")[0].innerHTML.toUpperCase().includes(searchBar.value.toUpperCase())) {
+            // -ära peida elementi
             item.classList.remove("hidden");
         } else {
+            // -peida element
             item.classList.add("hidden");
         }
     }
+
+    // Kui peidetud elemente on sama palju kui üldse elemente kokku (ehk kõik on peidetud)
+    if (grid.length == document.getElementsByClassName("hidden").length) {
+        noResultsText.style.display = "block";
+    } else {
+        noResultsText.style.display = "none";
+    }
 }
 
+// Event listener, et käivitata funktsioon igal klahvivajutusel, et otsimine oleks automaatne
 searchBar.addEventListener("keyup", () => {
     searchItems()
 });
-
