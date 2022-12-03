62a66336c8e9bd58843ce2a1a236de7a00065aaf
M <48407846+mvahaste@users.noreply.github.com>
2022-12-03 14:44:38 +0200

    Fixed '<br>' showing up in search, consistent code formatting

--- assets/browse.js
+++ assets/browse.js
@@ -11,24 +11,24 @@ let noResultsText = document.getElementsByClassName("no-results")[0];
 
 // Otsingufunktsioon
 function searchItems() {
-    // Käib üle iga elemendi
-    for (var item of grid) {
-        // Kui päring on / ei ole elemendi <p></p> elemendi sees-
-        if (item.getElementsByTagName("p")[0].innerHTML.toUpperCase().includes(searchBar.value.toUpperCase())) {
-            // -ära peida elementi
-            item.classList.remove("hidden");
-        } else {
-            // -peida element
-            item.classList.add("hidden");
-        }
-    }
+	// Käib üle iga elemendi
+	for (var item of grid) {
+		// Kui päring on / ei ole elemendi <p></p> elemendi sees-
+		if (item.getElementsByTagName("p")[0].innerHTML.toUpperCase().split("<BR>").join("").includes(searchBar.value.toUpperCase())) {
+			// -ära peida elementi
+			item.classList.remove("hidden");
+		} else {
+			// -peida element
+			item.classList.add("hidden");
+		}
+	}
 
-    // Kui peidetud elemente on sama palju kui üldse elemente kokku (ehk kõik on peidetud)
-    if (grid.length == document.getElementsByClassName("hidden").length) {
-        noResultsText.style.display = "block";
-    } else {
-        noResultsText.style.display = "none";
-    }
+	// Kui peidetud elemente on sama palju kui üldse elemente kokku (ehk kõik on peidetud)
+	if (grid.length == document.getElementsByClassName("hidden").length) {
+		noResultsText.style.display = "block";
+	} else {
+		noResultsText.style.display = "none";
+	}
 }
 
 //
@@ -37,5 +37,5 @@ function searchItems() {
 
 // Event listener, et käivitata funktsioon igal klahvivajutusel, et otsimine oleks automaatne
 searchBar.addEventListener("keyup", () => {
-    searchItems();
+	searchItems();
 });
--- assets/draw.js
+++ assets/draw.js
@@ -15,62 +15,62 @@ let currentColor = 0;
 //
 
 function generateGrid() {
-    // Kui gridSizeInput elemendil on / ei ole täisarvuline väärtus ja see on vahemikus 1-32-
-    if (gridSizeInput.value.length != 0) {
-        if (1 <= gridSizeInput.value && gridSizeInput.value <= 32 && Number.isInteger(Number(gridSizeInput.value))) {
-            // -määra väärtus gridSize muutujale
-            gridSize = gridSizeInput.value;
+	// Kui gridSizeInput elemendil on / ei ole täisarvuline väärtus ja see on vahemikus 1-32-
+	if (gridSizeInput.value.length != 0) {
+		if (1 <= gridSizeInput.value && gridSizeInput.value <= 32 && Number.isInteger(Number(gridSizeInput.value))) {
+			// -määra väärtus gridSize muutujale
+			gridSize = gridSizeInput.value;
 
-            // Võta ära error class, et border ei oleks punane
-            gridSizeInput.classList.remove("error");
-        } else {
-            // -muuda gridSizeInput elemendi border punaseks
-            gridSizeInput.classList.add("error");
-        }
-    }
+			// Võta ära error class, et border ei oleks punane
+			gridSizeInput.classList.remove("error");
+		} else {
+			// -muuda gridSizeInput elemendi border punaseks
+			gridSizeInput.classList.add("error");
+		}
+	}
 
-    // Tee grid tühjaks
-    grid.innerHTML = "";
+	// Tee grid tühjaks
+	grid.innerHTML = "";
 
-    // Korda gridSize^2 et grid ruut
-    for (var i = 0; i < gridSize ** 2; i++) {
-        // Tee uus div
-        let div = document.createElement("div");
-        // Anna sellele id
-        div.id = i;
-        // Lisa div element grid-i
-        grid.appendChild(div);
-    }
+	// Korda gridSize^2 et grid ruut
+	for (var i = 0; i < gridSize ** 2; i++) {
+		// Tee uus div
+		let div = document.createElement("div");
+		// Anna sellele id
+		div.id = i;
+		// Lisa div element grid-i
+		grid.appendChild(div);
+	}
 
-    // Muuda gridi column ja row repeat väärtust
-    root.style.setProperty("--grid-size", gridSize);
+	// Muuda gridi column ja row repeat väärtust
+	root.style.setProperty("--grid-size", gridSize);
 }
 
 function drawOnGrid(target) {
-    // Kui elemendil on / ei ole class transparent-
-    if (target.classList.contains("transparent")) {
-        // -võta class ära
-        target.classList.remove("transparent");
-    } else {
-        // -lisa class
-        target.classList.add("transparent");
-    }
+	// Kui elemendil on / ei ole class transparent-
+	if (target.classList.contains("transparent")) {
+		// -võta class ära
+		target.classList.remove("transparent");
+	} else {
+		// -lisa class
+		target.classList.add("transparent");
+	}
 }
 
 function changeColor() {
-    // Suurenda värvi indeksit
-    currentColor += 1;
+	// Suurenda värvi indeksit
+	currentColor += 1;
 
-    // Ära lase indeksil minna suuremaks kui colorList-i pikkus
-    if (currentColor >= colorList.length) {
-        currentColor = 0;
-    }
+	// Ära lase indeksil minna suuremaks kui colorList-i pikkus
+	if (currentColor >= colorList.length) {
+		currentColor = 0;
+	}
 
-    // Võta ära vana värv
-    grid.classList.remove(grid.classList[1]);
+	// Võta ära vana värv
+	grid.classList.remove(grid.classList[1]);
 
-    // Pane uus värv
-    grid.classList.add(colorList[currentColor]);
+	// Pane uus värv
+	grid.classList.add(colorList[currentColor]);
 }
 
 //
@@ -79,47 +79,47 @@ function changeColor() {
 
 // Kuula hiire vajutust
 document.body.addEventListener("mousedown", (e) => {
-    // Kui elemendi parent elemendi class on grid
-    if (e.target.parentElement.className.includes("grid")) {
-        // Joonista
-        drawOnGrid(e.target);
+	// Kui elemendi parent elemendi class on grid
+	if (e.target.parentElement.className.includes("grid")) {
+		// Joonista
+		drawOnGrid(e.target);
 
-        // Lisa elemendi id ignoreList-i
-        ignoreList.push(e.target.id);
+		// Lisa elemendi id ignoreList-i
+		ignoreList.push(e.target.id);
 
-        mouseDown = true;
-    }
+		mouseDown = true;
+	}
 });
 
 // Kuula hiire lahti laskmist
 document.body.addEventListener("mouseup", () => {
-    // Tühjenda ignoreList
-    ignoreList = [];
+	// Tühjenda ignoreList
+	ignoreList = [];
 
-    mouseDown = false;
+	mouseDown = false;
 });
 
 // Kuula hiire hover-it
 document.body.addEventListener("mouseover", (e) => {
-    if (e.target.parentElement.className.includes("grid")) {
-        // Kui elemendi id ei ole tühi ja ei ole ignoreList-is ja hiire nupp on alla vajutatud
-        if (e.target.id != "" && !ignoreList.includes(e.target.id) && mouseDown) {
-            // Joonista
-            drawOnGrid(e.target);
+	if (e.target.parentElement.className.includes("grid")) {
+		// Kui elemendi id ei ole tühi ja ei ole ignoreList-is ja hiire nupp on alla vajutatud
+		if (e.target.id != "" && !ignoreList.includes(e.target.id) && mouseDown) {
+			// Joonista
+			drawOnGrid(e.target);
 
-            // Lisa elemendi id ignoreList-i
-            ignoreList.push(e.target.id);
-        }
-    }
+			// Lisa elemendi id ignoreList-i
+			ignoreList.push(e.target.id);
+		}
+	}
 });
 
 // Kuula keyup eventi inputis
 gridSizeInput.addEventListener("keyup", (e) => {
-    // Kui nupp on enter-
-    if (e.key == "Enter") {
-        // -genereeri grid
-        generateGrid();
-    }
+	// Kui nupp on enter-
+	if (e.key == "Enter") {
+		// -genereeri grid
+		generateGrid();
+	}
 });
 
 //
